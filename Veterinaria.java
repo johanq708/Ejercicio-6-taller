f28ca79868c3bec85630bb7f25d02b832926d6a2
johit <johit@DESKTOP-HQPQDH2>
2017-04-16 21:06:55 -0500

    ejercicio 6 veterinaria

---
+++ Veterinaria.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio6;
+import java.util.ArrayList;
+/**
+ *
+ * @author johit
+ */
+public class Veterinaria {
+    private ArrayList<Mascota> mv;
+
+    public Veterinaria() {
+        this.mv = new ArrayList<>();
+    }
+    
+    public void adicionarmascota(Mascota m){
+        mv.add(m);
+    }
+    
+    public void eliminarmascota(Mascota m){
+        mv.remove(m);
+    }
+    
+    public void mostrarmascotaportipo(){
+        for (int i=0;i<mv.size();i++){
+            System.out.println(mv.get(i));
+        }
+    }
+    public void mostrarmascotas(){
+        for(int i=0;i<mv.size();i++){
+            System.out.println(mv.get(i));
+        }
+    }
+}
